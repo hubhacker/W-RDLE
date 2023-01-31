6cf29388190cdd7fd449c4ba5bf3589f2e61836e
jolene12345 <jolliu26@bergen.org>
2023-01-31 11:56:48 -0500

    worked on hard mode

--- hard_gamemode.py
+++ hard_gamemode.py
@@ -1,12 +1,18 @@
 from tkinter import *
 import random
 
-class Application(Frame):
-    def __init__(self, master):
-        super(Application, self).__init__(master)
-        self.grid()
-        self.create_widgets()
+class Grid:
+    
+    def create_widgets(self):
+        Label(self, text = 'Guess!:', font = 'Helvetica 15 bold', fg = "navy blue").grid(row = 'pass')
+        self.guess_ent = Entry(self)
+        self.guess_ent.grid(row = 'pass')
 
+class Word:
+    
+    def __init__(self, hardword):
+        self.hardword = hardword
+        
     def choose_word(self, file): 
         sixletter = open(file)
 
@@ -15,6 +21,15 @@ class Application(Frame):
         for word in sixletter:
             self.sixletterlist.append(word.strip())
 
-        self.word = self.sixletterlist([random.randint(0, 1000)])
+        self.hardword = self.sixletterlist([random.randint(0, 1000)])
 
+class Guess:
+    
+
+    guessed = False
+
+    while guessed == False:
+        
+        
+    
 
