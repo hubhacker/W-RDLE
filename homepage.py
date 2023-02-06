dc79b74f45f82bf254c9da7360729103bb4ab65d
hubhacker <shreyathirumala@gmail.com>
2023-02-06 12:56:03 -0500

    xhanes

--- homepage.py
+++ homepage.py
@@ -1,24 +1,37 @@
 from tkinter import *
+import tkinter as tk
 
 class Application(Frame):
     def __init__(self, master):
         super(Application, self).__init__(master)
         self.grid()
         self.create_widgets()
-    def create_widgets(self):
-        Label(self, text = "W ♾ RDLE", font=("Helvetica", 20),
-            width=20, fg = "green").grid(row=0, column=1, columnspan = 1, sticky=W+E)
         
-        Label(self, text= "Easy Mode!", font = "Century 13" , fg = "HotPink3").grid(row = 2, column = 1, )
-        Button()
+    def create_widgets(self):
+        Label(self, text = "W ♾ RDLE", font=("Helvetica", 60),
+            width=20, fg = "green").grid(row=0, column=1, columnspan = 1, sticky=N)
+        
+        #Label(self, text= "Easy Mode!", font = "Century 13" , fg = "HotPink3").grid(row = 5, column = 1)
+        Button(self, text = "Easy Mode!", command = self.easy_mode, font =, fg = ).grid(row = , column = , sticky = )
 
-        Label(self, text = "Intermediate Mode!", font = "Century 13", fg = "DarkOrchid2").grid(row = 2, column = 4)
-        Button()
+        #Label(self, text = "Intermediate Mode!", font = "Century 13", fg = "DarkOrchid2").grid(row = 5, column = 2)
+        Button(self, text = "Intermediate Mode!", command = self.intermediate_mode, font =, fg = ).grid(row = , column = , sticky = )
 
-        Label(self, text= "Hard Mode!", font = "Century 13", fg = "PeachPuff4").grid(row = 2, column = 7)
-        Button()
+        #Label(self, text= "Hard Mode!", font = "Century 13", fg = "PeachPuff4").grid(row = 5, column = 3)
+        Button(self, text = "Hard Mode!", command = self.hard_mode, font =, fg = ).grid(row = , column = , sticky = )
+
+
+    def easy_mode(self):
+        pass
+
+    def intermediate_mode(self):
+        pass
+
+    def hard_mode(self):
+        pass
         
 root = Tk()
+root = tk.Tk()
 root.title("Homepage")
 app = Application(root)
 root.mainloop()
