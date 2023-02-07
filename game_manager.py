b97688adec1843c7a3a94dcdf8e26fffae0c59de
sarahspe <sarspe26@bergen.org>
2023-02-07 13:17:13 -0500

    x

---
+++ game_manager.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+class GameManager (object):
+    def __init__(self):
+        self.root = Tk()
+        self.screen = None
+
+    def home_screen (self):
+        self.root.title ("Select game mode:")
+        self.difficulties
