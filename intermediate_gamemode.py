06f5b89f6b05784c4773175c535ccea1fb3b0067
hubhacker <shreyathirumala@gmail.com>
2023-02-09 13:25:16 -0500

    changes

--- intermediate_gamemode.py
+++ intermediate_gamemode.py
@@ -26,18 +26,21 @@ class Intermediate(Frame):
 
         Label(self, text= "INTERMEDIATE MODE").grid(row = 0, column= 1, columnspan= 1, sticky = W+E)
 
-        self.letter1 = Text(self, width = 100, height = 10, wrap = WORD)
-        self.letter1.grid(row = 8, column = 0, columnspan = 1)
-        self.letter2 = Text(self, width = 100, height = 10, wrap = WORD)
-        self.letter2.grid(row = 8, column = 1, columnspan = 1)
-        self.letter3 = Text(self, width = 100, height = 10, wrap = WORD)
-        self.letter3.grid(row = 8, column = 2, columnspan = 1)
-        self.letter4 = Text(self, width = 100, height = 10, wrap = WORD)
-        self.letter4.grid(row = 8, column = 3, columnspan = 1)
-        self.letter5 = Text(self, width = 100, height = 10, wrap = WORD)
-        self.letter5.grid(row = 8, column = 4, columnspan = 1)
+        self.letter1 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter1.grid(row = 5, column = 0, columnspan = 1)
+        self.letter2 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter2.grid(row = 5, column = 1, columnspan = 1)
+        self.letter3 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter3.grid(row = 5, column = 2, columnspan = 1)
+        self.letter4 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter4.grid(row = 5, column = 3, columnspan = 1)
+        self.letter5 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter5.grid(row = 5, column = 4, columnspan = 1)
 
-        Label(self, text = 'Guess: ').grid(row = 6, column = 0, sticky = W)
+
+        
+        
+
         self.guess_ent = Entry(self)
         self.guess_ent.grid(row = 6, column = 1, sticky = W, columnspan=1)
         
