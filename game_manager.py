41e850549069291a32d650529e2a8fa3a697a637
sarahspe <sarspe26@bergen.org>
2023-02-09 11:35:29 -0500

    cb

--- game_manager.py
+++ game_manager.py
@@ -9,7 +9,10 @@ class GameManager (object):
 
     def setup_homescreen (self):
         self.root.title ("Select game mode:")
-        self.screen = Application 
+        self.screen = Application(self, master= self.root
+                                  callback_hompage = self.onclose_homepage)
+    def onclose_homepage(self):
+        self.screen= Easy()
 def main():
     game = GameManager()
     game.setup_homescreen()
--- homepage.py
+++ homepage.py
@@ -6,7 +6,7 @@ from hard_gamemode import Application
 from shop import Application
 
 class Application(Frame):
-    def __init__(self, master, callback_on_easy, callback_on_intermediate, callback_on_hard):
+    def __init__(self, master, callback_on_easy):
         super(Application, self).__init__(master)
         self.grid()
         self.create_widgets()
@@ -21,13 +21,13 @@ class Application(Frame):
             width=20, fg = "green").grid(row=0, column=1, columnspan = 1, sticky=N)
         
         #Label(self, text= "Easy Mode!", font = "Century 13" , fg = "HotPink3").grid(row = 5, column = 1)
-        Button(self, text = "Easy Mode!", command = self., font =, fg = ).grid(row = , column = , sticky = )
+        Button(self, text = "Easy Mode!", command = self.setup_easy, font =, fg = ).grid(row = , column = , sticky = )
 
         #Label(self, text = "Intermediate Mode!", font = "Century 13", fg = "DarkOrchid2").grid(row = 5, column = 2)
-        Button(self, text = "Intermediate Mode!", command = self., font =, fg = ).grid(row = , column = , sticky = )
+        Button(self, text = "Intermediate Mode!", command = self.setup_intermediate, font =, fg = ).grid(row = , column = , sticky = )
 
         #Label(self, text= "Hard Mode!", font = "Century 13", fg = "PeachPuff4").grid(row = 5, column = 3)
-        Button(self, text = "Hard Mode!", command = self., font =, fg = ).grid(row = , column = , sticky = )
+        Button(self, text = "Hard Mode!", command = self.setup_hard, font =, fg = ).grid(row = , column = , sticky = )
 
 
     def setup_easy(self):
