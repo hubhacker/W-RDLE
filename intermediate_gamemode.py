1d58a0473cc97b7d080d25906cb3cd8a93ff6a91
hubhacker <shreyathirumala@gmail.com>
2023-02-10 12:56:31 -0500

    changes

--- intermediate_gamemode.py
+++ intermediate_gamemode.py
@@ -38,11 +38,50 @@ class Intermediate(Frame):
         self.letter5.grid(row = 5, column = 4, columnspan = 1)
 
 
-        
-        
+        self.letter6 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter6.grid(row = 11, column = 0, columnspan = 1)
+        self.letter7 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter7.grid(row = 11, column = 1, columnspan = 1)
+        self.letter8 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter8.grid(row = 11, column = 2, columnspan = 1)
+        self.letter9 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter9.grid(row = 11, column = 3, columnspan = 1)
+        self.letter10 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter10.grid(row = 11, column = 4, columnspan = 1)
 
-        self.guess_ent = Entry(self)
-        self.guess_ent.grid(row = 6, column = 1, sticky = W, columnspan=1)
+
+        self.letter11 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter11.grid(row = 17, column = 0, columnspan = 1)
+        self.letter12 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter12.grid(row = 17, column = 1, columnspan = 1)
+        self.letter13 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter13.grid(row = 17, column = 2, columnspan = 1)
+        self.letter14 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter14.grid(row = 17, column = 3, columnspan = 1)
+        self.letter15 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter15.grid(row = 17, column = 4, columnspan = 1)
+
+        self.letter16 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter16.grid(row = 23, column = 0, columnspan = 1)
+        self.letter17 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter17.grid(row = 23, column = 1, columnspan = 1)
+        self.letter18 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter18.grid(row = 23, column = 2, columnspan = 1)
+        self.letter19 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter19.grid(row = 23, column = 3, columnspan = 1)
+        self.letter20 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter20.grid(row = 23, column = 4, columnspan = 1)
+
+        self.letter21 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter21.grid(row = 29, column = 0, columnspan = 1)
+        self.letter22 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter22.grid(row = 29, column = 1, columnspan = 1)
+        self.letter23 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter23.grid(row = 29, column = 2, columnspan = 1)
+        self.letter24 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter24.grid(row = 29, column = 3, columnspan = 1)
+        self.letter25 = Text(self, width = 10, height = 5, wrap = WORD)
+        self.letter25.grid(row = 29, column = 4, columnspan = 1)
         
        
         
