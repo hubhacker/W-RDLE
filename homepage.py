60dbd7991316f1b7638af2d13b737ac5939a11b6
sarahspe <sarspe26@bergen.org>
2023-03-02 08:37:56 -0500

    bkhb

--- homepage.py
+++ homepage.py
@@ -16,18 +16,23 @@ class Application(Frame):
         #self.callback_on_easy = callback_on_easy
         
     def create_widgets(self):
+        canvas = Canvas()
+
         
         Label(root, text = "W ♾ RDLE", font=("Helvetica", 20), fg = "black").place(x=120,y=10)
         
         #Label(self, text= "Easy Mode!", font = "Century 13" , fg = "HotPink3").grid(row = 5, column = 1)
-        Button(root, text = "Easy Mode!", command = self.setup_easy, font ="Helvetica", fg = "white", bg = "green").place(x=140,y=60)
+        Button(root, text = "Easy Mode!", command = self.setup_easy, font ="Helvetica", fg = "black", bg = "#F9DCC4").place(x=140,y=60)
 
         # #Label(self, text = "Intermediate Mode!", font = "Century 13", fg = "DarkOrchid2").grid(row = 5, column = 2)
-        Button(root, text = "Intermediate Mode!", command = self.setup_intermediate, font ="Helvetica", fg = "white", bg= "blue").place(x=105, y=110)
+        Button(root, text = "Intermediate Mode!", command = self.setup_intermediate, font ="Helvetica", fg = "white", bg= "blue").place(x=110, y=110)
 
         # #Label(self, text= "Hard Mode!", font = "Century 13", fg = "PeachPuff4").grid(row = 5, column = 3)
-        Button(root, text = "Hard Mode!", command = self.setup_hard, font ="Helvetica", fg = "white", bg = "red").place(x=140,y=160)
+        Button(root, text = "Hard Mode!", command = self.setup_hard, font ="Helvetica", fg = "white", bg = "#4f7aa8").place(x=140,y=160)
 
+        canvas.create_line(2, 25, 500, 25, width=2)
+        canvas.create_line(2, 220, 500, 220, width=2)
+        canvas.pack()
 
     def setup_easy(self):
         pass
