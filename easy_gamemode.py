9427e0de464f50ba56e2d5063f19fe6f8e77ef52
hubhacker <shreyathirumala@gmail.com>
2023-03-03 12:51:58 -0500

    ajsdklsj

--- easy_gamemode.py
+++ easy_gamemode.py
@@ -86,24 +86,17 @@ class Easy(Frame):
         self.letter24.place(relx = 0.8, rely = 0.80, anchor = N)
 
 
-    def guess(self):
-        
-        pass
+    #def retrieve_word1(self, event):
 
-    def check_guess(self): # callback function
-        
-        guess = self.guess_ent.get()
-        
-        while guess != self.easyword:
-            pass
-        
-        word_single = []
-        for i in guess:
-            word_single.append(i)
-        letter = Label[i] = i
-        
-        self.delete(0.0, END)
-        self.letter1.insert(0.0, letter)
+        #l1 = self.letter1.get(1.0, "end-1c")
+        #l2 = self.letter2.get(1.0, "end-1c")
+        #l3 = self.letter3.get(1.0, "end-1c")
+        #l4 = self.letter4.get(1.0, "end-1c")
+        #l5 = self.letter5.get(1.0, "end-1c")
+        #l6 = self.letter6.get(1.0, "end-1c")
+
+        #word = l1 + l2 + l3 + l4 + l5 + l6
+        #return word
 
 root = Tk()
 root.title("EASY MODE")
